cdff164c07a682043576da921d8ff834f1f70a18
davidkatz <15Dkatz@shcp.edu>
2017-02-02 11:25:06 -0800

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  # Returns the Gravator for the given user
+  def gravatar_for(user, size: 80 )
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    # Must use double quotes in the below link, otherwise interpolation will fail.
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
