b72c1e35e99e3f18018465361b5abd0704db274f
davidkatz <15Dkatz@shcp.edu>
2017-02-05 14:15:58 -0800

    Add user following

---
+++ test/integration/following_test.rb
@@ -0,0 +1,36 @@
+class FollowingTest < ActionDispatch::IntegrationTest
+  def setup
+    @user  = users(:david)
+    @other = users(:samwise)
+    log_in_as(@user)
+  end
+
+  test "should follow a user the standard way" do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, params: { followed_id: @other.id }
+    end
+  end
+
+  test "should follow a user with Ajax" do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, xhr: true, params: { followed_id: @other.id }
+    end
+  end
+
+  test "should unfollow a user the standard way" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
+  test "should unfollow a user with Ajax" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship), xhr: true
+    end
+  end
+
+end
